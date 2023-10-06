5e346b5857c54ab4d489ccdf213cc41764ae511a
FrankoSav <117610367+FrankoSav@users.noreply.github.com>
2023-10-06 19:25:06 -0300

    Update posteo.py

--- posteo.py
+++ posteo.py
@@ -11,15 +11,21 @@ def main():
         return
 
     time.sleep(2)
+    # Primer Click para minimizar vscode o la ventana en que estes ejecutando el codigo
     pyautogui.click(1811, 24)
     time.sleep(2)
+    # Segundo click para presionar y escribir el posteo
     pyautogui.click(933, 470)
 
+    # Mensaje del posteo
     def posteo():
         return """
-    AQUI PON EL MENSAJE DE TU PUBLICACION!
+    AQUI PON EL MENSAJE DE TU POSTEO
         """
+    # Velocidad a la que se escribe el posteo
     pyautogui.write(posteo(), interval=0.00)
+    
+    # Click para publicar el posteo
     pyautogui.click(954, 905)
 
 
